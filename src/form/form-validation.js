313ac897e5f51b0bc7e6117bc5506c745e355e3e
Likit <likitchamuthai@gmail.com>
2023-05-26 10:55:16 +0700

    update

--- src/form/form-validation.js
+++ src/form/form-validation.js
@@ -10,6 +10,19 @@ export default function FormValidation() {
   return (
     <div className="container mt-5 p-3 rounded" style={{width:'500px', background:'#cee'}}>
         <form onSubmit={handleSubmit(onSubmitForm)}>
+             <div class="form-floating mb-3 mt-3">
+                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" 
+                    {...register('email',{required:true, pattern: {
+                            value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.([a-zA-Z]{2,4})$/i,
+                                }
+                            }
+                        )
+                    }
+                />
+                {errors.email && <div style={err}>กรุณาระบุรูปแบบ Email ให้ถูกต้อง</div>}
+                <label for="email">Email</label>
+            </div>
+            
             <div class="form-floating mb-3 mt-3">
                 <input type="text" class="form-control" id="name" placeholder="กรอกชื่อสินค้า" name="name"
                 {...register('name',{required:true, maxLength:30})}
